8db50f68ea805042cf8e48de34479552033b1937
bufdev <bufdev-github@buf.build>
2020-05-03 15:47:44 -0400

    addlicnese ignores

--- make/go/addlicense.mk
+++ make/go/addlicense.mk
@@ -12,8 +12,18 @@ $(call _assert_var,COPYRIGHT_YEAR)
 $(call _assert_var,LICENSE_TYPE)
 $(call _assert_var,ADDLICENSE)
 
+# Settable ADDLICENSE_IGNORES
+
 .PHONY: addlicense
-addlicense: $(ADDLICENSE)
-	addlicense -c "$(COPYRIGHT_OWNER)" -l "$(LICENSE_TYPE)" -y "$(COPYRIGHT_YEAR)" .
+addlicense: __addlicense_files $(ADDLICENSE)
+	$(foreach addlicense_file,$(sort $(ADDLICENSE_FILES)),addlicense -c "$(COPYRIGHT_OWNER)" -l "$(LICENSE_TYPE)" -y "$(COPYRIGHT_YEAR)" $(addlicense_file) || exit 1;)
 
 licensegenerate:: addlicense
+
+.PHONY: __addlicense_files
+__addlicense_files:
+ifdef ADDLICENSE_IGNORES
+	$(eval ADDLICENSE_FILES := $(shell git ls-files | grep -v $(patsubst %,-e %,$(sort $(ADDLICENSE_IGNORES)))))
+else
+	$(eval ADDLICENSE_FILES := .)
+endif
