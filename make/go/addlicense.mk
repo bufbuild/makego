02779e924ea28c6282cbc52cb0ba91c683a4527a
bufdev <bufdev-github@buf.build>
2020-05-03 14:37:04 -0400

    Add addlicense

---
+++ make/go/addlicense.mk
@@ -0,0 +1,20 @@
+# Managed by makego. DO NOT EDIT.
+
+# Must be set
+$(call _assert_var,MAKEGO)
+$(call _conditional_include,$(MAKEGO)/base.mk)
+$(call _conditional_include,$(MAKEGO)/dep_addlicense.mk)
+$(call _conditional_include,$(MAKEGO)/dep_protoc_gen_go.mk)
+# Must be set
+$(call _assert_var,COPYRIGHT_OWNER)
+# Must be set
+$(call _assert_var,COPYRIGHT_YEAR)
+# Must be set
+$(call _assert_var,LICENSE_TYPE)
+$(call _assert_var,ADDLICENSE)
+
+.PHONY: addlicense
+addlicense: $(ADDLICENSE)
+	addlicense -c "$(COPYRIGHT_OWNER)" -l "$(LICENSE_TYPE)" -y "$(COPYRIGHT_YEAR)"
+
+pregenerate:: addlicense
