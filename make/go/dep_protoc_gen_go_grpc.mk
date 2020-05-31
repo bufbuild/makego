8d89081d1a53850eb9c375c13f66668d7e35d061
bufdev <bufdev-github@buf.build>
2020-05-31 13:25:06 -0400

    Add protoc-gen-go-grpc and move from golang/protobuf to google.golang.org/protobuf

---
+++ make/go/dep_protoc_gen_go_grpc.mk
@@ -0,0 +1,27 @@
+# Managed by makego. DO NOT EDIT.
+
+# Must be set
+$(call _assert_var,MAKEGO)
+$(call _conditional_include,$(MAKEGO)/base.mk)
+$(call _assert_var,CACHE_VERSIONS)
+$(call _assert_var,GOBIN)
+
+# Settable
+# https://github.com/grpc/grpc-go/commits/master 20200529 checked 20200531
+# TODO: Update to a specific release when protoc-gen-go-grpc is released
+PROTOC_GEN_GO_GRPC_VERSION ?= 806b6fc5961483019a839ac031671b32a761ef3e
+
+GO_GET_PKGS := $(GO_GET_PKGS) google.golang.org/grpc@$(PROTOC_GEN_GO_GRPC_VERSION)
+
+PROTOC_GEN_GO_GRPC := $(CACHE_VERSIONS)/protoc-gen-go-grpc/$(PROTOC_GEN_GO_GRPC_VERSION)
+$(PROTOC_GEN_GO_GRPC):
+	@rm -f $(GOBIN)/protoc-gen-go-grpc
+	$(eval PROTOC_GEN_GO_GRPC_TMP := $(shell mktemp -d))
+	cd $(PROTOC_GEN_GO_GRPC_TMP); go get \
+		google.golang.org/grpc/cmd/protoc-gen-go-grpc@$(PROTOC_GEN_GO_GRPC_VERSION)
+	@rm -rf $(PROTOC_GEN_GO_GRPC_TMP)
+	@rm -rf $(dir $(PROTOC_GEN_GO_GRPC))
+	@mkdir -p $(dir $(PROTOC_GEN_GO_GRPC))
+	@touch $(PROTOC_GEN_GO_GRPC)
+
+dockerdeps:: $(PROTOC_GEN_GO_GRPC)
