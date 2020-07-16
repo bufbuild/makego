04ce6ab82f6c68784976f8101fdfecf3668dba35
bufdev <bufdev-github@buf.build>
2020-07-16 13:36:37 -0400

    Add Buf

--- make/go/dep_protoc_gen_go_grpc.mk
+++ make/go/dep_protoc_gen_go_grpc.mk
@@ -4,7 +4,7 @@
 $(call _assert_var,MAKEGO)
 $(call _conditional_include,$(MAKEGO)/base.mk)
 $(call _assert_var,CACHE_VERSIONS)
-$(call _assert_var,GOBIN)
+$(call _assert_var,CACHE_BIN)
 
 # Settable
 # https://github.com/grpc/grpc-go/commits/master 20200623 checked 20200624
@@ -15,9 +15,9 @@ GO_GET_PKGS := $(GO_GET_PKGS) google.golang.org/grpc@$(PROTOC_GEN_GO_GRPC_VERSIO
 
 PROTOC_GEN_GO_GRPC := $(CACHE_VERSIONS)/protoc-gen-go-grpc/$(PROTOC_GEN_GO_GRPC_VERSION)
 $(PROTOC_GEN_GO_GRPC):
-	@rm -f $(GOBIN)/protoc-gen-go-grpc
+	@rm -f $(CACHE_BIN)/protoc-gen-go-grpc
 	$(eval PROTOC_GEN_GO_GRPC_TMP := $(shell mktemp -d))
-	cd $(PROTOC_GEN_GO_GRPC_TMP); go get \
+	cd $(PROTOC_GEN_GO_GRPC_TMP); GOBIN=$(CACHE_BIN) go get \
 		google.golang.org/grpc/cmd/protoc-gen-go-grpc@$(PROTOC_GEN_GO_GRPC_VERSION)
 	@rm -rf $(PROTOC_GEN_GO_GRPC_TMP)
 	@rm -rf $(dir $(PROTOC_GEN_GO_GRPC))
--- make/go/protoc_gen_go_grpc.mk
+++ make/go/protoc_gen_go_grpc.mk
@@ -3,8 +3,10 @@
 # Must be set
 $(call _assert_var,MAKEGO)
 $(call _conditional_include,$(MAKEGO)/base.mk)
+$(call _conditional_include,$(MAKEGO)/dep_buf.mk)
 $(call _conditional_include,$(MAKEGO)/dep_protoc.mk)
 $(call _conditional_include,$(MAKEGO)/dep_protoc_gen_go_grpc.mk)
+$(call _conditional_include,$(MAKEGO)/protoc_gen_go.mk)
 # Must be set
 $(call _assert_var,PROTO_PATH)
 # Must be set
@@ -27,7 +29,7 @@ PROTOC_GEN_GO_GRPC_EXTRA_FLAGS := --use-buf --by-dir
 endif
 
 .PHONY: protocgengogrpc
-protocgengogrpc: protocgengoclean $(PROTOC) $(PROTOC_GEN_GO_GRPC)
+protocgengogrpc: protocgengoclean $(PROTOC) $(BUF) $(PROTOC_GEN_GO_GRPC)
 	bash $(MAKEGO)/scripts/protoc_gen_plugin.bash $(PROTOC_GEN_GO_GRPC_EXTRA_FLAGS) \
 		"--proto_path=$(PROTO_PATH)" \
 		"--proto_include_path=$(CACHE_INCLUDE)" \
