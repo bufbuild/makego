5f0e54f006075b790a8334fad4a5a3db52df2aa2
bufdev <bufdev-github@buf.build>
2020-10-08 10:38:22 -0400

    Switch from protoc to buf

--- make/go/protoc_gen_twirp.mk
+++ make/go/protoc_gen_twirp.mk
@@ -4,42 +4,22 @@
 $(call _assert_var,MAKEGO)
 $(call _conditional_include,$(MAKEGO)/base.mk)
 $(call _conditional_include,$(MAKEGO)/dep_buf.mk)
-$(call _conditional_include,$(MAKEGO)/dep_protoc.mk)
 $(call _conditional_include,$(MAKEGO)/dep_protoc_gen_twirp.mk)
 $(call _conditional_include,$(MAKEGO)/protoc_gen_go.mk)
 # Must be set
-$(call _assert_var,PROTO_PATH)
-# Must be set
 $(call _assert_var,PROTOC_GEN_TWIRP_OUT)
-$(call _assert_var,CACHE_INCLUDE)
-$(call _assert_var,PROTOC)
+$(call _assert_var,CACHE_BIN)
+$(call _assert_var,BUF)
 $(call _assert_var,PROTOC_GEN_TWIRP)
 
 # Not modifiable for now
 PROTOC_GEN_TWIRP_OPT := paths=source_relative
 
-EXTRA_MAKEGO_FILES := $(EXTRA_MAKEGO_FILES) scripts/protoc_gen_plugin.bash
-
-PROTOC_GEN_TWIRP_EXTRA_FLAGS :=
-ifdef USE_BUF_PROTOC
-PROTOC_GEN_TWIRP_EXTRA_FLAGS := --use-buf-protoc
-endif
-ifdef USE_BUF_PROTOC_BY_DIR
-PROTOC_GEN_TWIRP_EXTRA_FLAGS := --use-buf-protoc-by-dir
-endif
-ifdef USE_BUF_GENERATE
-PROTOC_GEN_TWIRP_EXTRA_FLAGS := --use-buf-generate
-endif
-
 .PHONY: protocgentwirp
-protocgentwirp: protocgengoclean $(PROTOC) $(BUF) $(PROTOC_GEN_TWIRP)
-	bash $(MAKEGO)/scripts/protoc_gen_plugin.bash $(PROTOC_GEN_TWIRP_EXTRA_FLAGS) \
-		"--buf_path=$(CACHE_BIN)/buf" \
-		"--proto_path=$(PROTO_PATH)" \
-		"--proto_include_path=$(CACHE_INCLUDE)" \
-		$(patsubst %,--proto_include_path=%,$(PROTO_INCLUDE_PATHS)) \
-		"--plugin_name=twirp" \
-		"--plugin_out=$(PROTOC_GEN_TWIRP_OUT)" \
-		"--plugin_opt=$(PROTOC_GEN_TWIRP_OPT)"
+protocgentwirp: protocgengoclean $(BUF) $(PROTOC_GEN_TWIRP)
+	$(CACHE_BIN)/buf beta generate \
+		--plugin twirp \
+		--plugin-out $(PROTOC_GEN_TWIRP_OUT) \
+		--plugin-opt $(PROTOC_GEN_TWIRP_OPT)
 
-protocgenerate:: protocgentwirp
+bufgenerate:: protocgentwirp
