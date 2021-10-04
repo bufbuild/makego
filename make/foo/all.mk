3833e1bfe572a1ec6a87a26eabb3a8bf1ae39eb5
bufdev <bufdev-github@buf.build>
2021-10-04 11:53:56 -0400

    Add license_header.mk

--- make/foo/all.mk
+++ make/foo/all.mk
@@ -1,10 +1,16 @@
 GO_BINS := $(GO_BINS) cmd/foo
 DOCKER_BINS := $(DOCKER_BINS) foo
 
+LICENSE_HEADER_LICENSE_TYPE := apache
+LICENSE_HEADER_COPYRIGHT_HOLDER := Buf Technologies, Inc.
+LICENSE_HEADER_YEAR_RANGE := 2020-2021
+LICENSE_HEADER_IGNORES := \/testdata
+
 include make/go/bootstrap.mk
 include make/go/go.mk
 include make/go/docker.mk
 include make/go/buf.mk
+include make/go/license_header.mk
 include make/go/dep_protoc_gen_go.mk
 
 bufgeneratedeps:: $(BUF) $(PROTOC_GEN_GO)
