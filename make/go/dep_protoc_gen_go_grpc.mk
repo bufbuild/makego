383cdab9b837b1fba0883948ff54ed20eedbd611
bufdev <bufdev-github@buf.build>
2022-02-24 15:52:01 -0500

    Update grpc-go version

--- make/go/dep_protoc_gen_go_grpc.mk
+++ make/go/dep_protoc_gen_go_grpc.mk
@@ -7,8 +7,8 @@ $(call _assert_var,CACHE_VERSIONS)
 $(call _assert_var,CACHE_BIN)
 
 # Settable
-# https://github.com/grpc/grpc-go/releases 20220125 checked 20220224
-PROTOC_GEN_GO_GRPC_VERSION ?= v1.44.0
+# https://github.com/grpc/grpc-go/releases 20220224 checked 20220224
+PROTOC_GEN_GO_GRPC_VERSION ?= 328efcc9276e51849caaf0be0c9d524d0e4fd1cb
 
 GO_GET_PKGS := $(GO_GET_PKGS) google.golang.org/grpc@$(PROTOC_GEN_GO_GRPC_VERSION)
 
