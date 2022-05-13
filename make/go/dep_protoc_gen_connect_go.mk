6f82dad28ac730d5432c6750aaf5498ad92626bf
Steve Ayers <smaye81@gmail.com>
2022-05-13 12:38:08 -0400

    Add dep_protoc_gen_connect_go Makefile (#23)
    * Added dep_protoc_gen_connect_go
    
    Co-authored-by: Steve Ayers <sayers@uber.com>

---
+++ make/go/dep_protoc_gen_connect_go.mk
@@ -0,0 +1,21 @@
+# Managed by makego. DO NOT EDIT.
+
+# Must be set
+$(call _assert_var,MAKEGO)
+$(call _conditional_include,$(MAKEGO)/base.mk)
+$(call _assert_var,CACHE_VERSIONS)
+$(call _assert_var,CACHE_BIN)
+
+# Settable
+# https://github.com/bufbuild/connect-go 20220512 checked 20220513
+CONNECT_VERSION ?= c35ee255c4f0265262da15a5f431d761a7498b93
+
+PROTOC_GEN_CONNECT_GO := $(CACHE_VERSIONS)/connect-go/$(CONNECT_VERSION)
+$(PROTOC_GEN_CONNECT_GO):
+	@rm -f $(CACHE_BIN)/connect-go
+	GOBIN=$(CACHE_BIN) go install github.com/bufbuild/connect-go/cmd/protoc-gen-connect-go@$(CONNECT_VERSION)
+	@rm -rf $(dir $(PROTOC_GEN_CONNECT_GO))
+	@mkdir -p $(dir $(PROTOC_GEN_CONNECT_GO))
+	@touch $(PROTOC_GEN_CONNECT_GO)
+
+dockerdeps:: $(PROTOC_GEN_CONNECT_GO)
