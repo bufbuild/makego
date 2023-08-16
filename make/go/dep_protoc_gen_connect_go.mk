a0d5b24b6d65d6c77e758199c361bd8cf36c50c6
Mike Fridman <mfridman@buf.build>
2023-08-16 13:59:58 -0400

    protoc-gen-connect-go

--- make/go/dep_protoc_gen_connect_go.mk
+++ make/go/dep_protoc_gen_connect_go.mk
@@ -15,7 +15,7 @@ GO_GET_PKGS := $(GO_GET_PKGS) \
 
 PROTOC_GEN_CONNECT_GO := $(CACHE_VERSIONS)/connect-go/$(CONNECT_VERSION)
 $(PROTOC_GEN_CONNECT_GO):
-	@rm -f $(CACHE_BIN)/connect-go
+	@rm -f $(CACHE_BIN)/protoc-gen-connect-go
 	GOBIN=$(CACHE_BIN) go install github.com/connectrpc/connect-go/cmd/protoc-gen-connect-go@$(CONNECT_VERSION)
 	@rm -rf $(dir $(PROTOC_GEN_CONNECT_GO))
 	@mkdir -p $(dir $(PROTOC_GEN_CONNECT_GO))
