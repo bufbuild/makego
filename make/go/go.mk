0974500c9023765470f8095d16d83ba2b0c494a1
John Chadwick <jchadwick@buf.build>
2024-02-21 13:47:03 -0500

    Use xargs to avoid long argument lists

--- make/go/go.mk
+++ make/go/go.mk
@@ -84,14 +84,14 @@ godeps: deps
 .PHONY: gofmtmodtidy
 gofmtmodtidy:
 	@echo gofmt -s -w ALL_GO_FILES
-	@gofmt -s -w $(shell find . -name '*.go')
+	@find . -name '*.go' -print0 | xargs -0 gofmt -s -w
 	go mod tidy -v
 
 format:: gofmtmodtidy
 
 .PHONY: checknonolint
 checknonolint:
-	@if grep '//nolint' $(shell find . -name '*.go'); then \
+	@if find . -name '*.go' -print0 | xargs -0 grep '//nolint'; then \
 		echo '//nolint directives found, surface ignores in .golangci.yml instead' >&2; \
 		exit 1; \
 	fi
