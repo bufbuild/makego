b629b0e8a6c1dfe69ed69f0e864c28bdb4b36c3d
Philip K. Warren <pwarren@buf.build>
2024-11-18 13:28:25 -0600

    Update protoc-gen-go to v1.35.2

--- make/go/dep_golangci_lint.mk
+++ make/go/dep_golangci_lint.mk
@@ -3,13 +3,12 @@
 # Must be set
 $(call _assert_var,MAKEGO)
 $(call _conditional_include,$(MAKEGO)/base.mk)
-$(call _conditional_include,$(MAKEGO)/dep_jq.mk)
 $(call _assert_var,CACHE_VERSIONS)
 $(call _assert_var,CACHE_BIN)
 
 # We want to ensure we rebuild golangci-lint every time we require a new Go minor version.
 # Otherwise, the cached version may not support the latest language features.
-GOLANGCI_LINT_GO_VERSION := $(shell go mod edit -json | jq -r .Go | cut -d'.' -f1-2)
+GOLANGCI_LINT_GO_VERSION := $(shell go list -m -f '{{.GoVersion}}' | cut -d'.' -f1-2)
 
 # Settable
 # https://github.com/golangci/golangci-lint/releases 20241110 checked 20241113
--- make/go/dep_protoc_gen_go.mk
+++ make/go/dep_protoc_gen_go.mk
@@ -7,8 +7,8 @@ $(call _assert_var,CACHE_VERSIONS)
 $(call _assert_var,CACHE_BIN)
 
 # Settable
-# https://github.com/protocolbuffers/protobuf-go/releases 20241007 checked 20241007
-PROTOC_GEN_GO_VERSION ?= v1.35.1
+# https://github.com/protocolbuffers/protobuf-go/releases 20241114 checked 20241118
+PROTOC_GEN_GO_VERSION ?= v1.35.2
 
 GO_GET_PKGS := $(GO_GET_PKGS) \
 	google.golang.org/protobuf/proto@$(PROTOC_GEN_GO_VERSION)
