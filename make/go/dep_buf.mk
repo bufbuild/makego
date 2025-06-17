8f16812dd3315b46bcc91a315c7689a901f37f0d
doriable <9167065+doriable@users.noreply.github.com>
2025-06-17 21:21:50 +0000

    Update bufbuild/buf version to 1.55.1

--- make/go/dep_buf.mk
+++ make/go/dep_buf.mk
@@ -8,7 +8,7 @@ $(call _assert_var,CACHE_BIN)
 
 # Settable
 # https://github.com/bufbuild/buf/releases 20250617 checked 20250617
-BUF_VERSION ?= v1.55.0
+BUF_VERSION ?= v1.55.1
 # Settable
 #
 # If set, this path will be installed every time someone depends on $(BUF)
