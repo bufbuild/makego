53ac624b1e2944270518f4de3dab060169123473
Doria Keung <doriable@users.noreply.github.com>
2026-04-17 18:52:48 -0400

    Update bufbuild/buf version to 1.68.2 (#301)

--- make/go/dep_buf.mk
+++ make/go/dep_buf.mk
@@ -7,8 +7,8 @@ $(call _assert_var,CACHE_VERSIONS)
 $(call _assert_var,CACHE_BIN)
 
 # Settable
-# https://github.com/bufbuild/buf/releases 20260414 checked 20260414
-BUF_VERSION ?= v1.68.1
+# https://github.com/bufbuild/buf/releases 20260417 checked 20260417
+BUF_VERSION ?= v1.68.2
 # Settable
 #
 # If set, this path will be installed every time someone depends on $(BUF)
