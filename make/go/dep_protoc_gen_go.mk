b8ff63f50f477b6327ad9a7db03e68c92274332a
Philip K. Warren <pkwarren@users.noreply.github.com>
2026-08-13 09:48:18 -0500

    Fix date comments

--- make/go/dep_protoc_gen_go.mk
+++ make/go/dep_protoc_gen_go.mk
@@ -7,7 +7,7 @@ $(call _assert_var,CACHE_VERSIONS)
 $(call _assert_var,CACHE_BIN)
 
 # Settable
-# https://github.com/protocolbuffers/protobuf-go/releases 20251212 checked 20260810
+# https://github.com/protocolbuffers/protobuf-go/releases 20260810 checked 20260813
 PROTOC_GEN_GO_VERSION ?= v1.36.12
 
 GO_GET_PKGS := $(GO_GET_PKGS) \
